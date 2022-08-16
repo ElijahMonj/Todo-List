ec9b518c8f37b312a94531ef59688765afdda716
Elijah Monjardin <monjardinelijah120@gmail.com>
2022-08-16 15:44:54 +0800

    added simple functionality

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function hideAddProject(){
     document.getElementById("add-project-form").style.display = "none";
     document.getElementById("cover").style.display = "none";
 }
+function showAddNote(){
+    document.getElementById("add-note").style.display = "flex";
+    document.getElementById("cover").style.display = "block";
+}
+function hideAddNote(){
+    document.getElementById("add-note").style.display = "none";
+    document.getElementById("cover").style.display = "none";
+}
