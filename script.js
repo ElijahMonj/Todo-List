d2b416012fd2f9d722d06f3d11fd80f0fa20b69d
Elijah Monjardin <monjardinelijah120@gmail.com>
2022-08-16 11:25:58 +0800

    added form

---
+++ script.js
@@ -0,0 +1,8 @@
+function showAddProject(){
+    document.getElementById("add-project-form").style.display = "flex";
+    document.getElementById("cover").style.display = "block";
+}
+function hideAddProject(){
+    document.getElementById("add-project-form").style.display = "none";
+    document.getElementById("cover").style.display = "none";
+}
